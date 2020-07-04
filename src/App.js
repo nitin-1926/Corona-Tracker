cf25c74491a46031d9685844af50971bc6da268f
nitin-1926 <nitin171538.cse@chitkara.edu.in>
2020-07-04 11:57:59 +0530

    Rebasing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
-import App from './App';
+class App extends React.Component {
+    render(){
+        return(
+            <div>
+                <h1>App</h1>
+            </div>
+        )
+    }
+}
 
-ReactDOM.render(<App />,document.getElementById('root'));
+export default App;
