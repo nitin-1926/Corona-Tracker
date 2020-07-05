4f0df3fe870dc98a823dcb9362c497b7902de779
nitin-1926 <nitin171538.cse@chitkara.edu.in>
2020-07-05 21:44:09 +0530

    Global Chart coded

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,8 +1,44 @@
-import React from 'react';
+import React,{ useState,useEffect } from 'react';
+import { fetchDailyData } from '../../api';
+import { Line, Bar } from 'react-chartjs-2';
 
 const Chart = () =>{
+    const [dailyData, setDailyData] = useState([]);
+
+    useEffect(() => {
+        const fetchAPI = async () =>{
+            setDailyData(await fetchDailyData());
+        }
+        fetchAPI();
+    });
+
+    const LineChart = (
+        dailyData.length
+            ? (
+                <Line 
+                data={{
+                    labels: dailyData.map(({ date }) => date),
+                    datasets: [{
+                        data: dailyData.map(({confirmed}) => confirmed),
+                        label: 'Infected',
+                        borderColor: '#3333ff',
+                        fill: true,
+                    }, {
+                        data: dailyData.map(({confirmed}) => confirmed),
+                        label: 'Deaths',
+                        borderColor: 'red',
+                        backgroundColor: 'rgba(225,0,0)',
+                        fill: true,
+                    }],
+                }}
+                />
+            ) : null
+        
+    )
     return (
-        <h1>Chart</h1>
+        <div className={StyleSheet.container}>
+            {LineChart}
+        </div>
     );
 };
 
